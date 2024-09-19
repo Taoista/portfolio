18e80a0cdb52e5ef90be1d7db4cf7be461efd1e6
taoista <taoista-@hotmail.com>
2024-09-19 06:29:47 -0300

    generando pagina de ficha de software

---
+++ src/services/proyectosController.ts
@@ -0,0 +1,203 @@
+const lista = [
+    {
+        id:1,
+        titulo:'Win optimizer Image', 
+        desc:'software para transformasar y optimizar imagenes',
+        desc_large:'',
+        desc_finish:'',
+        img: 'images/proyectos/win_optimizer_img.webp', 
+        repo:'https://github.com/Taoista/win_optimizer_img', 
+        download:"https://drive.google.com/file/d/1zBsAV7IifcQSIz56lPAMWz45tkUzpvO5/view?usp=sharing", 
+        logo:'images/proyectos/logo_win_optimizer_img.webp',
+        url:null,
+        tecnologies:[
+                'images/icons/flutter.svg',
+        ],
+        features:[
+            ''
+        ],
+        galery: [
+            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/02_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/03_wolverchile.webp'},
+        ]
+        },
+    {
+        id:2,
+        titulo:'wolverchile.cl', 
+        desc:'Landing page respresentantes oficial en chile',
+        desc_large:'Wolverchile.cl es una moderna landing page diseñada para presentar productos de manera atractiva y eficiente. Aunque no es una tienda en línea, su objetivo es conectar con el backend para mostrar un catálogo dinámico de productos. La página está optimizada para brindar una experiencia de usuario fluida y responsiva, adaptándose a cualquier dispositivo.',
+        desc_finish:'Wolverchile.cl es un ejemplo de cómo integrar tecnologías modernas para crear sitios web rápidos, eficientes y fáciles de mantener, enfocados en la presentación visual de productos de manera profesional.',
+        img: 'images/proyectos/wolverchile.webp', 
+        repo:null, 
+        download:null, 
+        logo:'images/proyectos/logo_wolverchile.webp',
+        url:'https://wolverchile.cl',
+        tecnologies:[
+            'images/icons/astro.svg',
+            'images/icons/vue.svg',
+        ],
+        features:[
+            'Desarrollada con Vue.js y Astro 4: Aprovechando la capacidad de Vue.js para crear interfaces de usuario interactivas y la velocidad de Astro para generar contenido estático, esta landing page ofrece una experiencia rápida y dinámica.',
+            'Estilos personalizados con Tailwind CSS: Utiliza Tailwind CSS para un diseño moderno, limpio y completamente adaptable, facilitando la personalización de los estilos y la coherencia visual en todo el sitio.',
+            'Conexión a un backend para visualización de productos: La landing page se conecta a un backend para obtener información actualizada de los productos y mostrarlos en tiempo real. Esto permite mantener el contenido actualizado sin necesidad de modificar el código de la página.',
+            'Optimización de rendimiento y SEO: Implementa prácticas recomendadas para garantizar un rendimiento rápido y una buena optimización para motores de búsqueda, mejorando la visibilidad en línea y la experiencia del usuario.',
+            'Diseño responsivo y adaptable: La página está diseñada para verse y funcionar perfectamente en cualquier dispositivo, desde smartphones hasta computadoras de escritorio.'
+        ],
+        galery: [
+            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/02_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/03_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/04_wolverchile.webp'},
+        ]
+        
+    },
+    {
+        id:3,
+        titulo:'geoclientes', 
+        desc:'Landing page respresentantes oficial en chile',
+        desc_large:'',
+        desc_finish:'',
+        img: 'images/proyectos/geo_cliente.webp', 
+        repo:null, 
+        download:null, 
+        logo:'images/proyectos/logo_geo_clientes.webp',
+        url:null,
+        tecnologies:[
+            'images/icons/flutter.svg',
+        ],
+        features:[
+            ''
+        ],
+        galery: [
+            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+        ]
+    },
+    {
+        id:4,
+        titulo:'Neumatruck', 
+        desc:'Ecommerce',
+        desc_large:'',
+        desc_finish:'',
+        img: 'images/proyectos/neumatruck.webp', 
+        repo:null, 
+        download:null, 
+        logo:'images/proyectos/logo_neumatruck.webp',
+        url:'https://neumatruck.cl',
+        tecnologies:[
+            'images/icons/laravel.svg',
+            'images/icons/vue.svg',
+        ],
+        features:[
+            ''
+        ],
+        galery: [
+            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+        ]
+    },
+    {
+        id:5,
+        titulo:'Neumaflotas', 
+        desc:'Software de gerenciamiento de flotas vehiculares',
+        desc_large:'',
+        desc_finish:'',
+        img: 'images/proyectos/neumaflotas.webp', 
+        repo:null, 
+        download:null, 
+        logo:'images/proyectos/logo_neumaflotas.webp',
+        url:null,
+        tecnologies:[
+            'images/icons/flutter.svg',
+        ],
+        features:[
+            ''
+        ],
+        galery: [
+            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+        ]
+    },
+    {
+        id:6,
+        titulo:'Premium Neumachile', 
+        desc:'E-commerce para distribuidores de productos automotrices',
+        desc_large:'',
+        desc_finish:'',
+        img: 'images/proyectos/premium.webp', 
+        repo:null, 
+        download:null, 
+        logo:'images/proyectos/logo_premium.webp',
+        url:null,
+        tecnologies:[
+            'images/icons/laravel.svg',
+            'images/icons/vue.svg',
+        ],
+        features:[
+            ''
+        ],
+        galery: [
+            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+        ]
+    },
+    {
+        id:7,
+        titulo:'Neumaequipos', 
+        desc:'E-commerce de venta de productos para serviteca',
+        desc_large:'',
+        desc_finish:'',
+        img: 'images/proyectos/neumaequipos.webp', 
+        repo:null, 
+        download:null, 
+        logo:'images/proyectos/logo_neumaequipos.webp',
+        url:null,
+        tecnologies:[
+            'images/icons/wordpress.svg',
+        ],
+        features:[
+            ''
+        ],
+        galery: [
+            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+        ]
+    },
+    {
+        id:8,
+        titulo:'Neumagenda', 
+        desc:'Software para agendar y comunicación interna',
+        desc_large:'',
+        desc_finish:'',
+        img: 'images/proyectos/neumagenda.webp', 
+        repo:null, 
+        download:null, 
+        logo:'images/proyectos/logo_neumagenda.webp',
+        url:null,
+        tecnologies:[
+            'images/icons/flutter.svg',
+        ],
+        features:[
+            ''
+        ],
+        galery: [
+            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+        ]
+    },
+];
+
+// * retorna el proyecto segun su ID
+export const getProyectById = async (id:number):Promise<any> =>{
+    for (const proyect of lista) {
+        if (proyect.id == id) {
+            return proyect;
+        }
+    }
+} 
+
+// * retorn atoda la lista del proyecto
+export const getProyectos = () =>{
+    return lista;
+}

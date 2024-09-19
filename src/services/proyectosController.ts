25ddfdfc6320e93d37ab3e40969284615ad6abf1
taoista <taoista-@hotmail.com>
2024-09-19 08:19:30 -0300

    creadno fichas de proyectos de software

--- src/services/proyectosController.ts
+++ src/services/proyectosController.ts
@@ -3,8 +3,8 @@ const lista = [
         id:1,
         titulo:'Win optimizer Image', 
         desc:'software para transformasar y optimizar imagenes',
-        desc_large:'',
-        desc_finish:'',
+        desc_large:'Win Optimizer Image es una potente aplicación de escritorio desarrollada en Flutter, destinada a la optimización eficiente de imágenes. Este software está diseñado para usuarios que buscan reducir el tamaño de sus archivos de imagen sin sacrificar la calidad, ideal para mejorar la velocidad de carga en sitios web y aplicaciones móviles.',
+        desc_finish:'Win Optimizer Image es la solución ideal para profesionales y entusiastas que necesitan una herramienta efectiva y fácil de usar para mejorar el rendimiento visual de sus proyectos mediante la optimización de imágenes.',
         img: 'images/proyectos/win_optimizer_img.webp', 
         repo:'https://github.com/Taoista/win_optimizer_img', 
         download:"https://drive.google.com/file/d/1zBsAV7IifcQSIz56lPAMWz45tkUzpvO5/view?usp=sharing", 
@@ -14,12 +14,14 @@ const lista = [
                 'images/icons/flutter.svg',
         ],
         features:[
-            ''
+            'Desarrollado con Flutter: Aprovecha la tecnología de Flutter para ofrecer una interfaz moderna y fluida, garantizando una experiencia de usuario intuitiva y consistente en diversas plataformas de escritorio.',
+            'Optimización avanzada de imágenes: Permite ajustar la calidad y el tamaño de las imágenes mediante múltiples opciones de compresión, equilibrando de manera óptima entre calidad visual y tamaño del archivo.',
+            'Interfaz intuitiva: Presenta una interfaz gráfica amigable que facilita la carga y optimización de imágenes con simples acciones, haciendo el proceso accesible incluso para usuarios no técnicos.',
+            'Compatibilidad con diversos formatos: Soporta una amplia gama de formatos de imagen, incluidos JPEG, PNG y GIF, proporcionando flexibilidad para adaptarse a diferentes necesidades de procesamiento.',
+            'Optimización en lote: Permite procesar múltiples imágenes al mismo tiempo, optimizando la eficiencia y ahorrando tiempo cuando se trabaja con grandes volúmenes de archivos.'
         ],
         galery: [
-            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
-            {type:false, img:'images/proyectos/wolverchile/02_wolverchile.webp'},
-            {type:false, img:'images/proyectos/wolverchile/03_wolverchile.webp'},
+           
         ]
         },
     {
@@ -56,8 +58,8 @@ const lista = [
         id:3,
         titulo:'geoclientes', 
         desc:'Landing page respresentantes oficial en chile',
-        desc_large:'',
-        desc_finish:'',
+        desc_large:'GeoClientes es una innovadora aplicación móvil desarrollada en Flutter para iOS y Android, diseñada para optimizar el proceso de captura de requerimientos de clientes y verificar visitas en terreno. Esta herramienta facilita a los vendedores la recopilación de información clave, la toma de fotografías y la obtención de la georreferencia exacta de su ubicación, garantizando una gestión eficiente y precisa de las visitas.',
+        desc_finish:'GeoClientes es la solución ideal para empresas que buscan mejorar la precisión y la eficiencia en la captura de datos de clientes y la gestión de visitas en terreno, proporcionando una herramienta integral y fácil de usar para sus equipos de ventas.',
         img: 'images/proyectos/geo_cliente.webp', 
         repo:null, 
         download:null, 
@@ -67,19 +69,25 @@ const lista = [
             'images/icons/flutter.svg',
         ],
         features:[
-            ''
+            'Desarrollada en Flutter para iOS y Android: Utiliza Flutter para ofrecer una experiencia de usuario fluida y consistente en ambas plataformas móviles, garantizando una interfaz intuitiva y de alto rendimiento.',
+            'Captura de requerimientos de clientes: Permite a los usuarios registrar de manera detallada las necesidades y requerimientos de los clientes, facilitando la recopilación de información esencial durante las visitas.',
+            'Toma de fotografías y georreferencia: Los usuarios pueden capturar fotos relevantes durante las visitas y obtener la georreferencia precisa de su ubicación, asegurando que cada visita sea verificada y documentada correctamente.',
+            'Sincronización con backend: Las imágenes y la información recopilada se almacenan de forma segura en un backend, proporcionando acceso centralizado a los datos y permitiendo su gestión y análisis posterior.',
+            'Verificación de visitas en terreno: La aplicación permite validar la presencia del vendedor en el lugar mediante la georreferencia, asegurando la exactitud de la información y la eficiencia en la gestión de visitas.',
+            'Interfaz amigable y eficiente: Diseñada con una interfaz intuitiva que simplifica el proceso de captura y gestión de datos, mejorando la productividad y la experiencia del usuario.'
         ],
         galery: [
-            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
-            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/geoclientes/01_geoclientes.webp'},
+            {type:false, img:'images/proyectos/geoclientes/02_geoclientes.webp'},
+            {type:false, img:'images/proyectos/geoclientes/03_geoclientes.webp'},
         ]
     },
     {
         id:4,
         titulo:'Neumatruck', 
         desc:'Ecommerce',
-        desc_large:'',
-        desc_finish:'',
+        desc_large:'Neumatruck es una eficiente y moderna página web de comercio electrónico desarrollada con Laravel y Vue.js, especializada en la venta de neumáticos para camiones. Esta plataforma está diseñada para ofrecer una experiencia de compra sencilla y accesible, permitiendo a cualquier usuario adquirir neumáticos sin necesidad de iniciar sesión.',
+        desc_finish:'Neumatruck proporciona una solución eficiente para la compra de neumáticos para camiones, combinando una tecnología avanzada con una interfaz de usuario sencilla y accesible para garantizar una experiencia de compra excepcional.',
         img: 'images/proyectos/neumatruck.webp', 
         repo:null, 
         download:null, 
@@ -90,19 +98,24 @@ const lista = [
             'images/icons/vue.svg',
         ],
         features:[
-            ''
+            'Desarrollada con Laravel y Vue.js: Utiliza Laravel para una sólida estructura backend y Vue.js para una interfaz de usuario dinámica y receptiva, asegurando un rendimiento óptimo y una experiencia de navegación fluida.',
+            'Compra sin necesidad de login: Los usuarios pueden explorar y comprar neumáticos sin tener que crear una cuenta o iniciar sesión, facilitando un proceso de compra rápido y sin fricciones.',
+            'Catálogo de neumáticos para camiones: Ofrece una amplia selección de neumáticos específicamente diseñados para camiones, con descripciones detalladas, especificaciones y opciones de filtrado para ayudar a los usuarios a encontrar el producto adecuado.',
+            'Interfaz intuitiva y fácil de usar: Diseñada con una interfaz amigable que simplifica la navegación y la búsqueda de productos, haciendo que la experiencia de compra sea lo más sencilla y eficiente posible.',
+            'Optimización para todos los dispositivos: La web está optimizada para funcionar perfectamente en dispositivos móviles y de escritorio, proporcionando una experiencia consistente y accesible en cualquier plataforma.'
         ],
         galery: [
-            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
-            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:true, img:'images/proyectos/neumatruck/01_neumatruck.webp'},
+            {type:false, img:'images/proyectos/neumatruck/02_neumatruck.webp'},
+            {type:false, img:'images/proyectos/neumatruck/03_neumatruck.webp'},
         ]
     },
     {
         id:5,
         titulo:'Neumaflotas', 
         desc:'Software de gerenciamiento de flotas vehiculares',
-        desc_large:'',
-        desc_finish:'',
+        desc_large:'NeumaFlotas es un avanzado software desarrollado en Flutter, diseñado para la gestión y registro de mediciones de neumáticos con el fin de facilitar el control gerencial. Este software permite a los usuarios registrar datos críticos de los neumáticos y enviar la información recopilada a un backend para su almacenamiento y análisis.',
+        desc_finish:'NeumaFlotas es la herramienta ideal para empresas que necesitan un sistema efectivo para gestionar las mediciones de neumáticos, mejorando la precisión en el control y mantenimiento de la flota mediante una solución tecnológica avanzada y fácil de usar.',
         img: 'images/proyectos/neumaflotas.webp', 
         repo:null, 
         download:null, 
@@ -112,42 +125,54 @@ const lista = [
             'images/icons/flutter.svg',
         ],
         features:[
-            ''
+            'Desarrollado en Flutter: Utiliza Flutter para proporcionar una interfaz de usuario moderna y fluida, garantizando un rendimiento consistente y una experiencia de usuario de alta calidad en dispositivos móviles.',
+            'Registro de mediciones de neumáticos: Permite a los usuarios registrar detalladamente las mediciones de neumáticos, facilitando el seguimiento y control del estado de los neumáticos en la flota.',
+            'Integración con backend: Toda la información registrada se envía y almacena de forma segura en un backend, permitiendo un acceso centralizado a los datos para su análisis y gestión gerencial.',
+            'Interfaz intuitiva y fácil de usar: Diseñado con una interfaz gráfica amigable que simplifica el proceso de registro y consulta de datos, haciendo que el manejo del software sea accesible y eficiente.',
+            'Optimización para dispositivos móviles: Asegura que el software funcione de manera óptima en smartphones y tablets, proporcionando flexibilidad para el uso en diferentes entornos de trabajo.'
         ],
         galery: [
-            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
-            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/neumaflotas/01_neumaflotas.webp'},
+            {type:false, img:'images/proyectos/neumaflotas/02_neumaflotas.webp'},
+            {type:false, img:'images/proyectos/neumaflotas/03_neumaflotas.webp'},
+            {type:false, img:'images/proyectos/neumaflotas/04_neumaflotas.webp'},
         ]
     },
     {
         id:6,
         titulo:'Premium Neumachile', 
         desc:'E-commerce para distribuidores de productos automotrices',
-        desc_large:'',
-        desc_finish:'',
+        desc_large:'premium.neumachile.cl es una plataforma de comercio electrónico avanzada, desarrollada en Angular con un backend en Lumen, especializada en la venta de productos vehiculares. Esta página ofrece una amplia gama de productos, incluyendo repuestos, aceites, neumáticos, baterías y más, para satisfacer las necesidades de los propietarios de vehículos y profesionales del sector.',
+        desc_finish:'premium.neumachile.cl es la solución ideal para quienes buscan una plataforma de comercio electrónico confiable y completa para adquirir productos vehiculares de alta calidad, combinando tecnología avanzada con una experiencia de usuario superior.',
         img: 'images/proyectos/premium.webp', 
         repo:null, 
         download:null, 
         logo:'images/proyectos/logo_premium.webp',
-        url:null,
+        url:'https://premium.neumachile.cl',
         tecnologies:[
             'images/icons/laravel.svg',
             'images/icons/vue.svg',
         ],
         features:[
-            ''
+            'Desarrollada en Angular: Utiliza Angular para una interfaz de usuario moderna, interactiva y receptiva, proporcionando una experiencia de navegación fluida y eficiente en todos los dispositivos.',
+            'Backend en Lumen: El backend en Lumen asegura un rendimiento rápido y eficiente, manejando las operaciones de servidor y la gestión de datos con alta disponibilidad y escalabilidad.',
+            'Catálogo completo de productos vehiculares: Ofrece una extensa selección de productos, incluyendo repuestos, aceites, neumáticos, baterías, y otros componentes esenciales para vehículos, con descripciones detalladas y opciones de filtrado.',
+            'Proceso de compra simplificado: Facilita un proceso de compra intuitivo y rápido, permitiendo a los usuarios encontrar y adquirir productos de manera sencilla, con opciones de pago seguras y eficaces',
+            'Optimización para todos los dispositivos: La web está diseñada para funcionar de manera óptima en dispositivos móviles y de escritorio, asegurando una experiencia consistente y accesible en cualquier plataforma.',
+            'Interfaz amigable y eficiente: Presenta una interfaz gráfica amigable que mejora la navegación y búsqueda de productos, haciendo que la experiencia de compra sea lo más eficiente posible.'
         ],
         galery: [
-            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
-            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:true, img:'images/proyectos/premium_neumachile/01_premium.webp'},
+            {type:true, img:'images/proyectos/premium_neumachile/02_premium.webp'},
+            {type:true, img:'images/proyectos/premium_neumachile/03_premium.webp'},
         ]
     },
     {
         id:7,
         titulo:'Neumaequipos', 
         desc:'E-commerce de venta de productos para serviteca',
-        desc_large:'',
-        desc_finish:'',
+        desc_large:'NeumaEquipos.cl es una tienda en línea desarrollada en WordPress, especializada en la venta de productos para servitecas. Esta plataforma está diseñada para ofrecer una amplia gama de equipos y suministros esenciales para el mantenimiento y reparación de vehículos, proporcionando una solución completa y accesible para profesionales del sector.',
+        desc_finish:'NeumaEquipos.cl proporciona una solución integral para servitecas que buscan una plataforma confiable para adquirir productos y equipos necesarios para su operación diaria, combinando la flexibilidad de WordPress con una experiencia de compra optimizada.',
         img: 'images/proyectos/neumaequipos.webp', 
         repo:null, 
         download:null, 
@@ -157,19 +182,24 @@ const lista = [
             'images/icons/wordpress.svg',
         ],
         features:[
-            ''
+            'Desarrollada en WordPress: Utiliza WordPress para una gestión de contenido flexible y fácil de usar, con una interfaz intuitiva que facilita la navegación y la administración del sitio.',
+            'Amplia gama de productos para servitecas: Ofrece una variedad de productos, incluyendo herramientas, equipos de diagnóstico, repuestos y otros suministros esenciales para el mantenimiento y reparación de vehículos.',
+            'Proceso de compra eficiente: Presenta un proceso de compra sencillo y directo, permitiendo a los usuarios encontrar y adquirir productos de manera rápida y segura, con opciones de pago confiables.',
+            'Diseño responsivo: Optimizado para funcionar perfectamente en dispositivos móviles y de escritorio, garantizando una experiencia de usuario consistente y accesible en cualquier plataforma.',
+            'Interfaz amigable y profesional: Diseñada con una interfaz clara y profesional que facilita la búsqueda y selección de productos, mejorando la experiencia general del usuario en el sitio.'
         ],
         galery: [
-            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
-            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:true, img:'images/proyectos/neumaequipos/01_neumaequipos.webp'},
+            {type:true, img:'images/proyectos/neumaequipos/02_neumaequipos.webp'},
+            {type:true, img:'images/proyectos/neumaequipos/03_neumaequipos.webp'},
         ]
     },
     {
         id:8,
         titulo:'Neumagenda', 
         desc:'Software para agendar y comunicación interna',
-        desc_large:'',
-        desc_finish:'',
+        desc_large:'NeumAgenda es una aplicación de software desarrollada en Flutter, diseñada para facilitar la programación y gestión de eventos y citas. Su objetivo principal es permitir a los usuarios agendar citas de cualquier tipo de manera sencilla y eficiente, a partir de los eventos y horarios designados por el administrador.',
+        desc_finish:'NeumAgenda es la solución ideal para empresas y organizaciones que necesitan una herramienta confiable para gestionar agendas y citas, combinando la tecnología de Flutter con una experiencia de usuario intuitiva y eficiente.',
         img: 'images/proyectos/neumagenda.webp', 
         repo:null, 
         download:null, 
@@ -179,11 +209,16 @@ const lista = [
             'images/icons/flutter.svg',
         ],
         features:[
-            ''
+            'Desarrollado en Flutter: Utiliza Flutter para proporcionar una interfaz de usuario moderna y fluida, asegurando un rendimiento óptimo y una experiencia consistente en dispositivos móviles y de escritorio.',
+            'Agendamiento de eventos: Permite a los usuarios programar citas y eventos en los horarios y fechas establecidos por el administrador, facilitando la organización y gestión de citas de manera efectiva.',
+            'Interfaz intuitiva: Diseñado con una interfaz gráfica amigable y fácil de usar, que simplifica el proceso de programación y visualización de citas, mejorando la eficiencia en la gestión de agendas.',
+            'Flexibilidad en la programación: Admite la programación de diferentes tipos de citas y eventos, adaptándose a diversas necesidades y escenarios de agendamiento.',
+            'Sincronización y notificaciones: Ofrece funciones de sincronización y notificación para recordar a los usuarios sobre las citas programadas y asegurar que no se pierdan eventos importantes.'
         ],
         galery: [
-            {type:true, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
-            {type:false, img:'images/proyectos/wolverchile/01_wolverchile.webp'},
+            {type:false, img:'images/proyectos/neumagenda/01_neumagenda.webp'},
+            {type:false, img:'images/proyectos/neumagenda/02_neumagenda.webp'},
+            {type:false, img:'images/proyectos/neumagenda/03_neumagenda.webp'},
         ]
     },
 ];
